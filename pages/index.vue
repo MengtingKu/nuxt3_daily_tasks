02a4054e82427db7b19a222179e6d5fc5c960a75
kuku <polorbear520@gmail.com>
2024-11-10 15:25:03 +0800

    Day 1 - 起手式 : 建立 Nuxt3 專案

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const route = useRoute();
+</script>
+
+<template>
+    <div>
+        Page: index
+        <p>目前路由的路徑 - route.fullPath 為：{{ route.path }}</p>
+    </div>
+</template>
+
+<style scoped></style>
