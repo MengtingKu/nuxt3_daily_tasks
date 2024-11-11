21d059b7457d454e4781362b643952435cffb53e
kuku <polorbear520@gmail.com>
2024-11-11 21:21:22 +0800

    Day 2 - Nuxt3 專案引入 CSS 樣式

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,18 @@ const route = useRoute();
         Page: index
         <p>目前路由的路徑 - route.fullPath 為：{{ route.path }}</p>
     </div>
+    <div>
+        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+        <button type="button" class="btn btn-warning">Warning</button>
+        <button type="button" class="btn btn-info">Info</button>
+        <button type="button" class="btn btn-light">Light</button>
+        <button type="button" class="btn btn-dark">Dark</button>
+
+        <button type="button" class="btn btn-link">Link</button>
+    </div>
 </template>
 
 <style scoped></style>
